e037898adb6218d31230120df11ea4ae93f96bfb
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-08-01 20:45:21 +1000

    added unordered list and corresponding variable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
