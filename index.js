483fb65281daa554e659944df9e1626927c14edb
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-10-12 14:10:21 +1100

    added functionality to delete button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const deleteBtn = document.querySelector("#delete-btn")
 const ulEl = document.querySelector("#ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
@@ -14,6 +15,12 @@ inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem ("myLeads", JSON.stringify(myLeads))
+    renderLeads()
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
