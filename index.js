a7cc7b56d33798e17eaed0513c65ef1ea78b14c0
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-10-16 14:02:00 +1100

    refactor renderLeads function to just render, updated all invocations of the function

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem ("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems +=`
             <li>
-                <a target='_blank' href='${myLeads}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads}'>
+                    ${leads[i]}
                 </a>
             </li>  
         `    
