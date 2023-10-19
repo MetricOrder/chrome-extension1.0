0694933e8d22b141c9f69afb5f8f7e47dbb99807
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-10-19 14:36:16 +1100

    changed terminology from leads to links - name from leads tracker to Bookmark Alternative

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 
-let myLeads = []
+let myLinks = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const tabBtn = document.querySelector("#tab-btn")
 const deleteBtn = document.querySelector("#delete-btn")
 const ulEl = document.querySelector("#ul-el")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
 
-if (leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+if (linksFromLocalStorage){
+    myLinks = linksFromLocalStorage
+    render(myLinks)
 }
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
+    myLinks.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem ("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    localStorage.setItem ("myLinks", JSON.stringify(myLinks))
+    render(myLinks)
 })
 
 tabBtn.addEventListener("click", function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem ("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
+        myLinks.push(tabs[0].url)
+        localStorage.setItem ("myLinks", JSON.stringify(myLinks))
+        render(myLinks)
     })
 })
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    myLinks = []
+    render(myLinks)
 })
 
-function render(leads) {
+function render(links) {
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
+    for (let i = 0; i < links.length; i++) {
         listItems +=`
             <li>
-                <a target='_blank' href='${leads}'>
-                    ${leads[i]}
+                <a target='_blank' href='${links}'>
+                    ${links[i]}
                 </a>
             </li>  
         `    
