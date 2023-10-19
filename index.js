312e69732218bbbec48befa77a777e783e6eeeea
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-10-19 14:27:09 +1100

    debugged - version is working

--- index.js
+++ index.js
@@ -19,18 +19,10 @@ inputBtn.addEventListener("click", function(){
     render(myLeads)
 })
 
-// tabBtn.addEventListener("click", function(){
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//         myLeads.push(tabs[0].url)
-//         localStorage.setItem ("myLeads", JSON.stringify(myLeads))
-//         render(myLeads)
-//     })
-// })
-
-tabBtn.addEventListener("click", function(){    
+tabBtn.addEventListener("click", function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        localStorage.setItem ("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     })
 })
